87dbb69795b761b8e6768967f0023eef050e2fa6
Aleksei Tcysin <24791800+tcysin@users.noreply.github.com>
2021-07-24 21:32:32 +0300

    Migrate homepage from old project

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
         "",
         TemplateView.as_view(template_name="home/home.html"),
         name="home",
-    )
+    ),
+    path(
+        "privacy-policy/",
+        TemplateView.as_view(template_name="home/privacy-policy.html"),
+        name="privacy-policy",
+    ),
 ]
