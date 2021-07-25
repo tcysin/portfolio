1789397d7a8f6cdec4e155de55952564ed5b1ed0
Aleksei Tcysin <24791800+tcysin@users.noreply.github.com>
2021-07-25 09:30:58 +0300

    Add production settings

---
+++ config/settings/production.py
@@ -0,0 +1,36 @@
+from .base import *  # noqa F403
+
+# GENERAL
+# -----------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")  # noqa F405
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = get_secret("DJANGO_ALLOWED_HOSTS")  # noqa F405
+
+
+# CACHES
+# -----------------------------------------------------------------------------
+# TODO
+
+
+# SECURITY
+# -----------------------------------------------------------------------------
+# TODO: see if the following apply on PAW
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+
+# EMAIL
+# -----------------------------------------------------------------------------
+# TODO
