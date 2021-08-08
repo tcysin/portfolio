cfc0849c99f51a493c2f1f080f83aaaf862ba1d6
Aleksei Tcysin <24791800+tcysin@users.noreply.github.com>
2021-08-08 15:03:23 +0300

    Add success page

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from .forms import ContactForm
 class ContactView(FormView):
     template_name = "home/contact.html"
     form_class = ContactForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("success")
 
     def form_valid(self, form):
         """If the form is valid, send email and redirect to success url."""
